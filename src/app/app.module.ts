0928d6a7f1c8e2dfb20b212c938e27bc028faece
Nathan Colpron <colproman@gmail.com>
2019-04-03 23:22:51 -0400

    4 view components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PackageComponent } from './package/package.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    PropertiesComponent,
+    ClientsComponent,
+    PackageComponent
   ],
   imports: [
     BrowserModule
